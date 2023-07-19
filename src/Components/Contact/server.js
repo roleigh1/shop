95dc4c02eee622504f7465dc1d1570c19725c1bb
roleigh1 <robin.leigh@outlook.de>
2023-07-19 21:45:38 +0200

    Update server.js

--- src/Components/Contact/server.js
+++ src/Components/Contact/server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(
+    3000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
     service: "gmail",
