284be748fe5281b6fef0670f4df677f20adee9d8
roleigh1 <robin.leigh@outlook.de>
2023-08-25 23:52:04 +0200

    styled the cartPage a little

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useCart } from "../CartContext";
-import { Button, Container } from 'react-bootstrap';
+import {  Container } from 'react-bootstrap';
 import { Row, Col } from 'react-bootstrap';
 import Logo from "../Home/Components/Logo/Logo";
 import BurgerMenu from "../Home/Components/Burger/Menu";
@@ -32,18 +32,18 @@ function Checkout() {
     </thead>
     <tbody>
         {cart.map((item, index) => (
-            <tr key={index}>
+            <tr key={index} style={{ borderBottom:' 1px solid #000', verticalAlign: 'middle' }}>
              
                 <td><h5>{item.name}</h5></td>
                 
-                <td><p> €{item.price}</p></td>
-                <td style={{display:'flex',gap:'1rem'}}>
-                <Button  onClick={() => updateQuantity(item.name, -1)}>-</Button>
+                <td><p style={{marginTop:'1rem'}}> €{item.price}</p></td>
+                <td style={{display:'flex',gap:'1rem' , marginTop:'1rem'}}>
+                <button style={{fontWeight:'bold',height:'1.5rem', width:'1rem',borderRadius:'50%',display: 'flex',justifyContent:'center',alignItems:'center',paddingBottom:'10px'  }} className="btn btn-danger" onClick={() => updateQuantity(item.name, -1)}>-</button>
                 <p>{item.quantity}</p>
-                <Button onClick={() => updateQuantity(item.name, 1)}>+</Button>
+                <button className='btn btn-success' style={{fontWeight:'bold',height:'1.5rem',borderRadius:'50%', width:'1rem',display: 'flex',justifyContent:'center',alignItems:'center', paddingBottom:'10px'}} onClick={() => updateQuantity(item.name, 1)}>+</button>
                 </td>
                 <td><div>€{(item.price * item.quantity).toFixed(2)}</div></td>
-                <td><Button onClick={() => removeFromCart(item.name)}>X</Button></td>
+                <td><button  style={{fontWeight:'bold',height:'1.5rem',borderRadius:'50%', width:'1.5rem',display: 'flex',justifyContent:'center',alignItems:'center',paddingBottom:'9px'}} className='btn btn-danger' onClick={() => removeFromCart(item.name)}>x</button></td>
             </tr>
         ))}
     </tbody>
