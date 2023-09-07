e3332897aae89f1d74e1c9ea0271b1275ad89ee5
roleigh1 <robin.leigh@outlook.de>
2023-09-07 23:29:47 +0200

    finishing working on float precision

--- src/Checkout/CheckoutTable.js
+++ src/Checkout/CheckoutTable.js
@@ -2,10 +2,13 @@ import React from "react";
 import { useCart } from "../CartContext";
 import { Row, Col } from "react-bootstrap";
 import { MDBBtn } from "mdb-react-ui-kit";
+import Decimal from "decimal.js";
 export default function CartTable() {
-
     const { cart, updateQuantity, removeFromCart } = useCart();
-    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0) + '€';
+    const totalValue = cart.reduce((acc, item) => {
+        return new Decimal(acc).plus(new Decimal(item.price).times(item.quantity));
+    }, 0).toFixed(2);
+
 
     return (
         <Row>
@@ -14,7 +17,7 @@ export default function CartTable() {
                 <thead>
 
                     <tr>
-                        <th><p style={{position:'relative',top:'8px',left:'1rem'}}>Items</p></th>
+                        <th><p style={{ position: 'relative', top: '8px', left: '1rem' }}>Items</p></th>
                         <th>Price</th>
                         <th></th>
                         <th>Total</th>
@@ -32,7 +35,7 @@ export default function CartTable() {
                                 <p>{item.quantity}</p>
                                 <button className='btn btn-success' style={{ fontWeight: 'bold', height: '1.5rem', borderRadius: '50%', width: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '10px' }} onClick={() => updateQuantity(item.name, 1)}>+</button>
                             </td>
-                            <td><div>{(item.price * item.quantity).toFixed(2)}€</div></td>
+                            <td><div>{(item.price * (item.quantity).toFixed(2))}€</div></td>
                             <td><button style={{ fontWeight: 'bold', height: '1.5rem', borderRadius: '50%', width: '1.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '9px' }} className='btn btn-danger' onClick={() => removeFromCart(item.name)}>x</button></td>
                         </tr>
                     ))}
@@ -45,7 +48,7 @@ export default function CartTable() {
                     <MDBBtn color="danger" style={{ width: '7rem', height: '2rem', textTransform: 'none', paddingTop: '0px', color: 'white', fontWeight: 'bold', letterSpacing: '1px', }}>Checkout</MDBBtn>
                 </Col>
                 <Col className="d-flex justify-content-center">
-                    <p style={{ marginLeft: '-2.3rem', }}>Total:</p><span style={{ textDecoration: 'underline' }}>{total}</span>
+                    <p style={{ marginLeft: '-2.3rem', }}>Total:</p><span style={{ textDecoration: 'underline' }}>{totalValue}</span>
                 </Col>
 
             </Row>
