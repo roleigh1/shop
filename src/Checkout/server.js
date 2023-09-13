a7366f4c4ddbaabf0effdfe4eefa8e21f644da68
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-09-14 00:08:28 +0200

    Update server.js

--- src/Checkout/server.js
+++ src/Checkout/server.js
@@ -6,7 +6,7 @@ const Stripe = require("stripe");
 
 const stripe = new Stripe("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");
 const app = express();
-const endpointSecret = "whsec_91c9d54c6ad7e73607868c34061ec1182e340c9155571f9104ab9902b2a7319b";
+const endpointSecret = "";
 
 app.use(cors());
 app.use(bodyParser.json());
