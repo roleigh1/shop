70bf6efa97299cedcf5a7b9d0de465a5d1997ba7
roleigh1 <robin.leigh@outlook.de>
2024-02-09 23:22:25 +0100

    Update ProductList.js

--- src/ProductPage/ProductList.js
+++ src/ProductPage/ProductList.js
@@ -5,22 +5,19 @@ import {
     MDBCol,
     
 } from "mdb-react-ui-kit";
-import ProductItem from "./ProductItem";
-import "./style.css"
 
+import "./style.css"
+import ProductItem from "./ProductItem";
 export default function ProductList({ products }) {
 
     const [selectedType, setSelectedType] = useState("");
     const [visibleProducts, setVisibleProducts] = useState(6)
     let filteredProducts;
-    if (!Array.isArray(products)) {
-        console.error("Invalid products prop provided to ProductList");
-        return null; // or return a fallback UI
-    }
+ 
     if (selectedType === "") {
         filteredProducts = [...products];
 
-        const typesOrder = ["vegetables", "fruit", "berrys", "herbs", "shroomes"];
+        const typesOrder = ["vegetables", "fruits", "berrys", "herbs", "shroomes"];
         filteredProducts.sort((a, b) => typesOrder.indexOf(a.type) - typesOrder.indexOf(b.type));
     } else {
         filteredProducts = products.filter(product => product.type === selectedType);
@@ -28,6 +25,8 @@ export default function ProductList({ products }) {
     const loadMoreProducts = () => {
         setVisibleProducts(prevCount => prevCount + 6);
     }
+    console.log(products); 
+    console.log("Filtered Products:", filteredProducts);
     return (
         <MDBContainer fluid className=" text-center ">
             <MDBRow>
@@ -35,7 +34,7 @@ export default function ProductList({ products }) {
                     <p style={{ position: "relative", top: "0.3rem" }}><strong>Categorys:</strong></p>
                     <select value={selectedType} style={{ marginTop: "-0.2rem", borderRadius: "0", border: "1px solid rgba(0, 0, 0, 0.3)", backgroundColor: "white" }} onChange={e => setSelectedType(e.target.value)}>
                         <option value="">All</option>
-                        {["vegetables", "shroomes", "berrys", "fruit", "herbs"].map(type => (
+                        {["vegetables", "shroomes", "berrys", "fruits", "herbs"].map(type => (
                             <option key={type} value={type}>{type.charAt(0).toUpperCase() + type.slice(1)}</option>
                         ))}
                     </select>
